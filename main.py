f7e965e64fa25599b3b0b395771b5df619bc3544
DouglasIde <douglasymide@gmail.com>
2025-03-18 08:41:17 -0300

    Feat: Creating the route manager and functions to calculate the distance and draw it

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
+import matplotlib.pyplot as plt
+from scipy.spatial.distance import euclidean as distancia_euclidiana
+from itertools import permutations
+
+
 if __name__ == "__main__":
-    print("Hello, World!")
+    
+
+    origem = (0,0)
+    destino = (4,4)
+    enderecos = [(2,1), (2,2), (8,3), (5,3)]
+
+    rota = [origem] + enderecos + [destino]
+
+    print(rota)
