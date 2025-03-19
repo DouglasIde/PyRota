266e36b5d233372cbbd40a5eb44c4a30b2b68798
DouglasIde <douglasymide@gmail.com>
2025-03-19 08:00:19 -0300

    Feat: Trying to find the shortest route between a set of addresses with brute force

--- main.py
+++ main.py
@@ -26,15 +26,37 @@ def desenhar_rota(rota):
     plt.title(f"Rota com distância de {round(distancia_percorrida, 2)}")
     plt.show()
 
+def calcular_distancia_rota(rota):
+    distancia = 0
+    tamanho_rota = len(rota)
+    for i in range(tamanho_rota):
+        if i < tamanho_rota - 1:
+            distancia += distancia_euclidiana(rota[i], rota[i+1])
+        return distancia
+
+
 if __name__ == "__main__":
     
     # Definição de origem, destino e pontos intermediários
     origem = (0,0)
     destino = (4,4)
     enderecos = [(2,1), (2,2), (8,3), (5,3)]
-    rota = [origem] + enderecos + [destino]
+    menor_distancia = float('inf')
+    menor_rota = None
 
-    desenhar_rota(rota)
+    for permutacao in permutations(enderecos):
+        rota = [origem] + enderecos + [destino]
+        distancia_rota = calcular_distancia_rota(rota)
+
+        if distancia_rota < menor_distancia:
+            menor_distancia = distancia_rota
+            menor_rota = rota
+
+    if menor_rota is None:
+        print("Nenhuma rota foi encontrada")
+    else:
+        print(menor_rota, menor_distancia)
+        desenhar_rota(menor_rota)
 
 
 
